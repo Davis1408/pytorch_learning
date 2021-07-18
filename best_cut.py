bd989c530d755f5b756d040b2eaa6c0cb118948e
Davis1408 <mhjdd67@163.com>
2021-07-18 18:53:12 +0800

    second up

--- best_cut.py
+++ best_cut.py
@@ -4,6 +4,7 @@ Created on Tue Jul  4 11:40:46 2017
 
 @author: Hank Kuang
 @title: 最优分箱&最优降基
+hello
 """
 
 import pandas as pd
@@ -20,7 +21,7 @@ def _check_target_binary(y):
     y:exog variable,pandas Series contains binary variable
     ------------------------------
     Return
-    if y is not binary, raise a error   
+    if y is not binary, raise a error
     """
     y_type = type_of_target(y)
     if y_type not in ['binary']:
@@ -32,7 +33,7 @@ def _isNullZero(x):
     check x is null or equal zero
     -----------------------------
     Params
-    x: data 
+    x: data
     -----------------------------
     Return
     bool obj
@@ -52,10 +53,10 @@ def _Gvalue(binDS, method):
     -----------------------------------------
     Return
     M_value: float or np.nan
-    """    
+    """
     R = binDS['bin'].max()
     N = binDS['total'].sum()
-    
+
     N_mat = np.empty((R,3))
     # calculate sum of 0,1
     N_s = [binDS[0].sum(), binDS[1].sum()]
@@ -66,7 +67,7 @@ def _Gvalue(binDS, method):
         N_mat[i][0] = subDS[0].sum()
         N_mat[i][1] = subDS[1].sum()
         N_mat[i][2] = subDS['total'].sum()
-    
+
     # Gini
     if method == 1:
         G_list = [0]*R
@@ -78,7 +79,7 @@ def _Gvalue(binDS, method):
         G = 0
         for j in range(2):
             G = G + N_s[j]*N_s[j]
-        
+
         G = 1 - G/(N*N)
         Gr = 0
         for i in range(int(R)):
@@ -90,19 +91,19 @@ def _Gvalue(binDS, method):
             for j in range(2):
                 if np.isnan(N_mat[i][j]) or N_mat[i][j] == 0:
                     M_value = 0
-        
+
         E_list = [0]*R
         for i in range(int(R)):
             for j in range(2):
                 E_list[i] = E_list[i] - ((N_mat[i][j]/float(N_mat[i][2]))\
                        *np.log(N_mat[i][j]/N_mat[i][2]))
-                
+
             E_list[i] = E_list[i]/np.log(2)#plus
         E = 0
         for j in range(2):
             a = (N_s[j]/N)
             E = E - a*(np.log(a))
-            
+
         E = E/np.log(2)
         Er = 0
         for i in range(2):
@@ -118,7 +119,7 @@ def _Gvalue(binDS, method):
             for j in range(2):
                 M[i][j] = N_mat[i][2]*N_s[j]/N
                 X2 = X2 + (N_mat[i][j]-M[i][j]) * (N_mat[i][j]-M[i][j]) / (M[i][j])
-        
+
         M_value = X2
     # Info value
     else:
@@ -128,12 +129,12 @@ def _Gvalue(binDS, method):
                 _isNullZero(N_s[1])]):
             M_value = np.NaN
         else:
-            IV =0 
+            IV =0
             for i in range(int(R)):
                 IV = IV + (N_mat[i][0]/N_s[0] - N_mat[i][1]/N_s[1])\
                     *np.log((N_mat[i][0]*N_s[1])/(N_mat[i][1]*N_s[0]))
             M_value = IV
-            
+
     return M_value
 
 
@@ -142,14 +143,14 @@ def _calCMerit(temp, ix, method):
     Calculation of the merit function for the current table temp
     ---------------------------------------------
     Params
-    temp: pandas dataframe, temp table in _bestSplit 
-    ix: single int obj,index of temp, from length of temp 
+    temp: pandas dataframe, temp table in _bestSplit
+    ix: single int obj,index of temp, from length of temp
     method: int obj, metric to split x(1:Gini, 2:Entropy, 3:person chisq, 4:Info value)
     ---------------------------------------------
     Return
     M_value: float or np.nan
     """
-    # split data by ix 
+    # split data by ix
     temp_L = temp[temp['i'] <= ix]
     temp_U = temp[temp['i'] > ix]
     # calculate sum of 0, 1, total for each splited data
@@ -176,9 +177,9 @@ def _calCMerit(temp, ix, method):
     elif method == 2:
         N = n_1s + n_2s
         E1= -((n_11/n_1s)*(np.log((n_11/n_1s))) + \
-        (n_12/n_1s)*(np.log((n_12/n_1s)))) / (np.log(2)) 
+        (n_12/n_1s)*(np.log((n_12/n_1s)))) / (np.log(2))
         E2= -((n_21/n_2s)*(np.log((n_21/n_2s))) + \
-         (n_22/n_2s)*(np.log((n_22/n_2s))))/(np.log(2)) 
+         (n_22/n_2s)*(np.log((n_22/n_2s))))/(np.log(2))
         E = -(((n_s1/N)*(np.log((n_s1/N))) + ((n_s2/N)*\
             np.log((n_s2/N))))/ (np.log(2)))
         M_value = 1-(n_1s*E1 + n_2s*E2)/(N*E)
@@ -191,9 +192,9 @@ def _calCMerit(temp, ix, method):
             for j in range(2):
                 M[i][j] = N_mat[i][2]*N_s[j]/N
                 X2 = X2 + ((N_mat[i][j]-M[i][j])*(N_mat[i][j]-M[i][j]))/M[i][j]
-        
+
         M_value = X2
-    # Info Value    
+    # Info Value
     else:
         try:
             IV = ((n_11/n_s1) - (n_12/n_s2)) * np.log((n_11*n_s2)/(n_12*n_s1)) \
@@ -202,7 +203,7 @@ def _calCMerit(temp, ix, method):
         except ZeroDivisionError:
             M_value = np.nan
     return M_value
-    
+
 
 def _bestSplit(binDS, method, BinNo):
     """
@@ -218,14 +219,14 @@ def _bestSplit(binDS, method, BinNo):
     Return
     newbinDS: pandas dataframe
     """
-    binDS = binDS.sort_values(by=['bin','pdv1'])  
+    binDS = binDS.sort_values(by=['bin','pdv1'])
     mb = len(binDS[binDS['bin']==BinNo])
-       
+
     bestValue = 0
     bestI = 1
     for i in range(1, mb):
         # split data by i
-        # metric: Gini,Entropy,pearson chisq,Info value 
+        # metric: Gini,Entropy,pearson chisq,Info value
         value = _calCMerit(binDS, i, method)
         # if value>bestValue，then make value=bestValue，and bestI = i
         if bestValue < value:
@@ -246,7 +247,7 @@ def _bestSplit(binDS, method, BinNo):
 
 def _candSplit(binDS, method):
     """
-    Generate all candidate splits from current Bins 
+    Generate all candidate splits from current Bins
     and select the best new bins
     middle procession functions for binContVar & reduceCats
     ---------------------------------------------
@@ -259,7 +260,7 @@ def _candSplit(binDS, method):
     newBins: pandas dataframe, split results
     """
     # sorted data by bin&pdv1
-    binDS = binDS.sort_values(by=['bin','pdv1'])    
+    binDS = binDS.sort_values(by=['bin','pdv1'])
     # get the maximum of bin
     Bmax = max(binDS['bin'])
     # screen data and cal nrows by diffrence bin
@@ -278,10 +279,10 @@ def _candSplit(binDS, method):
     bin_i_value = []
     for i in range(1, Bmax+1):
         if m[i] > 1: # if nrows of bin > 1
-            # split data by best i        
-            temp_trysplit[i] = _bestSplit(temp_binC[i], method, i)            
-            temp_trysplit[i]['bin'] = np.where(temp_trysplit[i]['split']==1, 
-                                               Bmax+1, 
+            # split data by best i
+            temp_trysplit[i] = _bestSplit(temp_binC[i], method, i)
+            temp_trysplit[i]['bin'] = np.where(temp_trysplit[i]['split']==1,
+                                               Bmax+1,
                                                temp_trysplit[i]['bin'])
             # delete bin == i
             temp_main[i] = binDS[binDS['bin']!=i]
@@ -296,12 +297,12 @@ def _candSplit(binDS, method):
     #binNum = temp_all_Vals['BinToSplit']
     binNum = bin_i_value[0][0]
     newBins = temp_main[binNum].drop('split', axis=1)
-    return newBins.sort_values(by=['bin', 'pdv1']) 
+    return newBins.sort_values(by=['bin', 'pdv1'])
 
 
 def _EqualWidthBinMap(x, Acc, adjust):
     """
-    Data bining function, 
+    Data bining function,
     middle procession functions for binContVar
     method: equal width
     Mind: Generate bining width and interval by Acc
@@ -326,13 +327,13 @@ def _EqualWidthBinMap(x, Acc, adjust):
     for i in ind:
         Upper[i] = varMin + i*minMaxSize
         Lower[i] = varMin + (i-1)*minMaxSize
-    
-    # adjust the min_bin's lower and max_bin's upper     
+
+    # adjust the min_bin's lower and max_bin's upper
     Upper[Mbins] = Upper[Mbins]+adjust
     Lower[1] = Lower[1]-adjust
     bin_map = pd.concat([Lower, Upper], axis=1)
     bin_map.index.name = 'bin'
-    return bin_map    
+    return bin_map
 
 
 def _applyBinMap(x, bin_map):
@@ -347,23 +348,23 @@ def _applyBinMap(x, bin_map):
     bin_res: pandas Series, result of bining
     """
     bin_res = np.array([0] * x.shape[-1], dtype=int)
-    
+
     for i in bin_map.index:
         upper = bin_map['upper'][i]
         lower = bin_map['lower'][i]
         x1 = x[np.where((x >= lower) & (x <= upper))[0]]
         mask = np.in1d(x, x1)
         bin_res[mask] = i
-    
+
     bin_res = pd.Series(bin_res, index=x.index)
     bin_res.name = x.name + "_BIN"
-    
+
     return bin_res
 
 def _combineBins(temp_cont, target):
     """
     merge all bins that either 0 or 1 or total =0
-    middle procession functions for binContVar 
+    middle procession functions for binContVar
     ---------------------------------
     Params
     temp_cont: pandas dataframe, middle results of binContVar
@@ -374,7 +375,7 @@ def _combineBins(temp_cont, target):
     """
     for i in temp_cont.index:
         rowdata = temp_cont.ix[i,:]
-        
+
         if i == temp_cont.index.max():
             ix = temp_cont[temp_cont.indexi].index.min()
         if any(rowdata[:3] == 0):#如果0,1,total有一项为0，则运行
@@ -388,7 +389,7 @@ def _combineBins(temp_cont, target):
             else:
                 temp_cont.ix[ix,'upper'] = rowdata['upper']
             temp_cont = temp_cont.drop(i, axis=0)
-        
+
     return temp_cont.sort_values(by='pdv1')
 
 
@@ -408,7 +409,7 @@ def _getNewBins(sub, i):
     total = sub['total'].sum()
     first = sub.iloc[0,:]
     last = sub.iloc[l-1,:]
-    
+
     lower = first['lower']
     upper = last['upper']
     df = pd.DataFrame()
@@ -420,14 +421,14 @@ def _getNewBins(sub, i):
 def binContVar(x, y, method, mmax=5, Acc=0.01, target=1, adjust=0.0001):
     """
     Optimal binings for contiouns var x by (y & method)
-    method is represent by number, 
+    method is represent by number,
         1:Gini, 2:Entropy, 3:person chisq, 4:Info value
     ---------------------------------------------
     Params
     x: pandas Series, which need to reduce category
     y: pandas Series, 0-1 distribute dependent variable
     method: int obj, metric to split x
-    mmax: int, bining number 
+    mmax: int, bining number
     Acc: float less than 1, partition ratio for equal width bining
     badlabel: target label
     adjust: float or np.inf, bining adjust for limitation
@@ -447,17 +448,17 @@ def binContVar(x, y, method, mmax=5, Acc=0.01, target=1, adjust=0.0001):
     # calculate freq of bin, and combine with t1
     t2 = temp_df.groupby(bin_res.name).count().ix[:,0]
     t2 = pd.DataFrame(t2)
-    t2.columns = ['total'] 
+    t2.columns = ['total']
     t = pd.concat([t1, t2], axis=1)
     # merge t & bin_map by t,
     # if all(0,1,total) == 1, so corresponding row will not appear in temp_cont
-    temp_cont = pd.merge(t, bin_map, 
-                         left_index=True, right_index=True, 
+    temp_cont = pd.merge(t, bin_map,
+                         left_index=True, right_index=True,
                          how='left')
     temp_cont['pdv1'] = temp_cont.index
     # if any(0,1,total)==0, then combine it with per bin or next bin
     temp_cont = _combineBins(temp_cont, target)
-    # calculate other temp vars      
+    # calculate other temp vars
     temp_cont['bin'] = 1
     temp_cont['i'] = range(1, len(temp_cont)+1)
     temp_cont['var'] = temp_cont.index
@@ -466,7 +467,7 @@ def binContVar(x, y, method, mmax=5, Acc=0.01, target=1, adjust=0.0001):
     while(nbins < mmax):
         temp_cont = _candSplit(temp_cont, method=method)
         nbins += 1
-       
+
     temp_cont = temp_cont.rename(columns={'var':'oldbin'})
     temp_Map1 = temp_cont.drop([0, target ,'pdv1' , 'i'], axis=1)
     temp_Map1 = temp_Map1.sort_values(by=['bin', 'oldbin'])
@@ -481,7 +482,7 @@ def binContVar(x, y, method, mmax=5, Acc=0.01, target=1, adjust=0.0001):
             rowdata = _getNewBins(sub_Map, i)
             data = data.append(rowdata, ignore_index=True)
             s.add(i)
-    
+
     # resort data
     data = data.sort_values(by='lower')
     data['newbin'] = range(1, mmax+1)
@@ -504,11 +505,11 @@ def _groupCal(x, y, badlabel=1):
     temp_cont: group calulate table
     m: nrows of temp_cont
     """
-    
+
     temp_cont = pd.crosstab(index=x, columns=y, margins=False)
     temp_cont['total'] = temp_cont.sum(axis=1)
     temp_cont['pdv1'] = temp_cont[badlabel]/temp_cont['total']
-    
+
     temp_cont['i']= range(1, temp_cont.shape[0]+1)
     temp_cont['bin'] = 1
     m = temp_cont.shape[0]
@@ -518,7 +519,7 @@ def _groupCal(x, y, badlabel=1):
 def reduceCats(x, y,  method=1, mmax=5, badlabel=1):
     """
     Reduce category for x by y & method
-    method is represent by number, 
+    method is represent by number,
         1:Gini, 2:Entropy, 3:person chisq, 4:Info value
     ----------------------------------------------
     Params:
@@ -532,12 +533,12 @@ def reduceCats(x, y,  method=1, mmax=5, badlabel=1):
     temp_cont: pandas dataframe, reduct category map
     """
     _check_target_binary(y)
-    temp_cont, m = _groupCal(x, y, badlabel=badlabel) 
+    temp_cont, m = _groupCal(x, y, badlabel=badlabel)
     nbins = 1
     while(nbins< mmax):
         temp_cont = _candSplit(temp_cont, method=method)
         nbins += 1
-    
+
     temp_cont = temp_cont.rename(columns={'var':x.name})
     temp_cont = temp_cont.drop([0, 1, 'i', 'pdv1'], axis=1)
     return temp_cont.sort_values(by='bin')
@@ -559,7 +560,7 @@ def applyMapCats(x, bin_map):
         subData = bin_map[bin_map.index==i]
         value = subData.ix[i,'bin']
         d[i] = value
-    
+
     new_x = x.map(d)
     new_x.name = x.name+'_BIN'
     return new_x

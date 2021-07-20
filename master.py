c028524ac4eaeb493ecb7f1bce634eaee2034e7b
bruce_trie <bruce_trie@bruce-trie.local>
2021-07-20 15:57:24 +0800

    master in code

---
+++ master.py
@@ -0,0 +1,1 @@
+master in code to edit
--- wifi_90%.py
+++ wifi_90%.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'offline code in this part'
+'online code in this part'
 import pandas as pd
 import numpy as np
 from sklearn import  preprocessing

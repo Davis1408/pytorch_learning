641b269b5a8ec708fe261c15414c292c8cf6a30e
bruce_trie <bruce_trie@bruce-trie.local>
2021-07-20 17:29:31 +0800

    offline test

--- best_cut.py
+++ best_cut.py
@@ -9,9 +9,10 @@ write by bruce
 hello add in new code
 davis new code
 dev in correct, jixukaifa dev cut new code
-hot_fix in code new add the new function, add in 07-200304 continue develop the dev branch 0406
-new code in 0417, new code in 0508
+
+offline test code in this project
 """
+
 
 import pandas as pd
 import numpy as np
